67fc990e1e2f5dbd0167cbe0fe1c3c81095da814
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-20 13:31:06 -0300

    base finish completed

---
+++ Curupira/Assets/Finish.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField]
+    int countEnemies = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            countEnemies--;
+
+            if (countEnemies <= 0)
+            {
+                print("Voce Venceu");
+            }
+        }
+    }
+}

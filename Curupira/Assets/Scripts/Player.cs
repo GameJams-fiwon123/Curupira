fd026fd3b8244af976f4c11eaf85e618f5fd9d08
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-20 11:28:41 -0300

    Implements Move player

--- Curupira/Assets/Scripts/Player.cs
+++ Curupira/Assets/Scripts/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour
     [SerializeField] float speed = 100;
 
     private Rigidbody2D rb;
-    private Vector2 motion = new Vector2();
+    private List<Vector2> motions = new List<Vector2>();
+
+    private Vector3 savePosition;
+    private Vector3 newPosition = Vector3.zero;
 
     // Start is called before the first frame update
     void Awake()
@@ -16,18 +19,111 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        savePosition = transform.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        motion.x = Input.GetAxisRaw("Horizontal");
-        motion.y = Input.GetAxisRaw("Vertical");
+        Inputs();
+        Move();
+    }
 
-        rb.velocity = motion.normalized * Time.deltaTime * speed;
+    private void Inputs()
+    {
+        Vector2 motion = Vector2.zero;
+
+        if (Input.GetButtonDown("left"))
+        {
+            motion.x = -1;
+        }
+        else if (Input.GetButtonDown("right"))
+        {
+            motion.x = 1;
+        }
+        else if (Input.GetButtonDown("up"))
+        {
+            motion.y = 1;
+        }
+        else if (Input.GetButtonDown("down"))
+        {
+            motion.y = -1;
+        }
+
+        if (motion.x != 0)
+        {
+            motions.Add(motion);
+        }
+        else if (motion.y != 0)
+        {
+            motions.Add(motion);
+        }
+
+
+    }
+
+    void Move()
+    {
+        GetNextPosition();
+        ProcessMove();
+    }
+
+    void GetNextPosition()
+    {
+        if (motions.Count > 0 && newPosition == Vector3.zero)
+        {
+            savePosition = transform.position;
+
+            newPosition = transform.position;
+            newPosition.x += motions[0].x;
+            newPosition.y += motions[0].y;
+        }
+    }
+
+    void ProcessMove()
+    {
+        if (newPosition != Vector3.zero)
+        {
+            Vector3 motion = Vector3.zero;
+            motion.x = motions[0].x / 10;
+            motion.y = motions[0].y / 10;
+
+            transform.position = transform.position + motion;
+            //rb.velocity = motions[0].normalized * Time.deltaTime * speed;
+            //float distance = Vector2.Distance(transform.position, newPosition);
+
+            //print(transform.position + "  " + newPosition);
+            if (transform.position == newPosition)
+            {
+                motions.RemoveAt(0);
+                newPosition = Vector3.zero;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameManager.instance.GameOver();
-        print("oi");
+        if (collision.gameObject.tag != "Enemy")
+        {
+            transform.position = savePosition;
+            motions.RemoveAt(0);
+            newPosition = Vector3.zero;
+        }
+        else
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
+    IEnumerator Walk(Vector3 newPosition)
+    {
+        
+
+        while (transform.position != newPosition)
+            yield return new WaitForSeconds(0);
+
+        rb.velocity = Vector2.zero;
     }
 }

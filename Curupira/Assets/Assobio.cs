a6705222e090d5da749ac1319c9bd1d403061f85
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-22 03:50:48 -0300

    Implement base Assobio

---
+++ Curupira/Assets/Assobio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assobio : MonoBehaviour
+{
+
+    private Animator anim;
+    private bool canUse = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && canUse)
+        {
+            anim.SetBool("canUse", true);
+            canUse = false;
+        }
+    } 
+
+    public void EnableUse()
+    {
+        canUse = true;
+    }
+
+    public void DisableUse()
+    {
+        anim.SetBool("canUse", false);
+    }
+}

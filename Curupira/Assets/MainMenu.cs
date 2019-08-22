3cee7a90e497e76d1a301c6ec2d10d2893ca5f4a
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-22 15:01:18 -0300

    Finish Main Menu

---
+++ Curupira/Assets/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    Button[] options;
+
+    private int index = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            index--;
+            if (index < 0)
+            {
+                index = options.Length - 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            index++;
+
+            if (index == options.Length)
+            {
+                index = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            switch (index)
+            {
+                case 0:
+                    FindObjectOfType<LevelManager>().LoadNextLevel();
+                    break;
+                case 1:
+                    FindObjectOfType<LevelManager>().LoadCredits();
+                    break;
+                case 2:
+                    FindObjectOfType<LevelManager>().ExitGame();
+                    break;
+            }
+        }
+
+        options[index].Select();
+    }
+}

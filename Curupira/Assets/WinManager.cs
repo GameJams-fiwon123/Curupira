2b3c9c967b07e82ea626a83586d0fb5e41f0dce2
fiwon123 <felipeinoue@yahoo.com.br>
2019-08-25 15:53:22 -0300

    Finish final scene

---
+++ Curupira/Assets/WinManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicManager.instance.PlayFinal(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
